8ab97ec83b0626d584c816caa7799b4e77e61cb9
Eduardo Vargas <117415684+vargased94@users.noreply.github.com>
2024-07-09 10:16:14 -0600

    Update section and hero styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function ({ addBase, config }) {
+			addBase({
+				'h1': { fontSize: config('theme.fontSize.2xl') },
+				'h2': { fontSize: config('theme.fontSize.xl') },
+				'h3': { fontSize: config('theme.fontSize.lg') },
+				'h4': { fontSize: config('theme.fontSize.base') },
+				'h5': { fontSize: config('theme.fontSize.sm') },
+				'h6': { fontSize: config('theme.fontSize.xs') },
+			})
+		}),
+	],
 }
